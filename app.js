c8e202e2b592a03221b34bdab284e58dd9431d10
Snehil Hota <snehilhota13032006@gmail.com>
2025-08-23 22:21:13 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const byId = document.getElementById('para');
+console.log(byId);
+const byClass = document.getElementsByClassName('highlight');
+console.log(byClass);
+const byTag = document.getElementsByTagName('p');
+console.log(byTag);
+const byName = document.getElementsByName('language');
+console.log(byName);
